f0e4b05a87ba1869240579c14b7d31c32770e9d3
hojoon90 <hojoon90@gmail.com>
2024-05-06 23:10:08 +0900

    연산자 계산

---
+++ src/main/kotlin/operator/ComparisonOperator.kt
@@ -0,0 +1,25 @@
+package com.basic.operator
+
+import kotlin.random.Random
+
+class ComparisonOperator {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            var a = Random.nextInt(10)
+            var b = Random.nextInt(10)
+            println("a : $a")
+            println("b : $b")
+
+            println(a > b)
+            println(a >= b)
+            println(a < b)
+            println(a <= b)
+            println(a == b)
+            println(a != b)
+
+        }
+
+    }
+}
