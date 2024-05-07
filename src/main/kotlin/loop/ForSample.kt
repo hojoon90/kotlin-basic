78abdf4b490471e065aabe1256e3433b8b46d76d
hojoon90 <hojoon90@gmail.com>
2024-05-07 23:40:31 +0900

    condition/loop

---
+++ src/main/kotlin/loop/ForSample.kt
@@ -0,0 +1,18 @@
+package com.basic.loop
+
+class ForSample {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            for (i in 1.. 5) println(i)
+            println()
+
+            for(i in 5 downTo 1) println(i)
+            println()
+
+            for (i in 1.. 5 step 2) println(i)
+            println()
+        }
+    }
+
+}
