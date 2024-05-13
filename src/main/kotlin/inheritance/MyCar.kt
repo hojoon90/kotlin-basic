3a132cff7b948d410f6dc131bd0c76e1a4a6305d
hojoon90 <hojoon90@gmail.com>
2024-05-13 23:47:27 +0900

    extends

---
+++ src/main/kotlin/inheritance/MyCar.kt
@@ -0,0 +1,11 @@
+package com.basic.inheritance
+
+import java.time.LocalDate
+
+class MyCar(name:String, price:Double, brand:String, private val purchaseDate: LocalDate)
+	:Car(name, price, brand) {
+	override fun myPurchaseDate() {
+		println("you made a purchase on $purchaseDate")
+	}
+
+}
